161eb987675a0d3e64f8c0564ac90dc0cd81c65b
Sarah Greene <sarahmaudegreene@gmail.com>
2015-04-01 17:51:12 -0400

    working model with one player

--- lib/battleship_controller.rb
+++ lib/battleship_controller.rb
@@ -1,78 +1,170 @@
 require_relative 'battleship_models'
 require 'pry'
 
-puts "Welcome to Battleship!"
-puts "Play (small) or (large) game?"
-size = gets.chomp
 
-puts "Please enter (1) or (2) players"
+puts " _______  _______  _______  _______  ___      _______  _______  __   __  ___   _______"
+puts "|  _    ||   _   ||       ||       ||   |    |       ||       ||  | |  ||   | |       |"
+puts "| |_|   ||  |_|  ||_     _||_     _||   |    |    ___||  _____||  |_|  ||   | |    _  |"
+puts "|       ||       |  |   |    |   |  |   |    |   |___ | |_____ |       ||   | |   |_| |"
+puts "|  _   | |       |  |   |    |   |  |   |___ |    ___||_____  ||       ||   | |    ___|"
+puts "| |_|   ||   _   |  |   |    |   |  |       ||   |___  _____| ||   _   ||   | |   | "
+puts "|_______||__| |__|  |___|    |___|  |_______||_______||_______||__| |__||___| |___|"
 
-board = Board.new(size)
-board.add_ships(size)
 
-puts "Enemy ships are in the water!"
+puts "(resume) game or (new) game?"
+res_or_new = gets.chomp
+    if res_or_new == "new"
+      Player1.delete_all
+      Player2.delete_all
+
+      puts "Please enter (1) or (2) players"
+      players = gets.chomp
+
+      board = Board.new
+      board.add_ships
+      board.print_board
+
+      @turnhistory = []
+      @hitcounter = []
+      @ship1_hits = []
+      @ship2_hits = []
+      @ship3_hits = []
+      @ship4_hits = []
+
+      @turnhistory2 = []
+      @hitcounter2 = []
+      @ship1_hits2 = []
+      @ship2_hits2 = []
+      @ship3_hits2 = []
+      @ship4_hits2 = []
+
+    elsif res_or_new =="resume"
+
+      @turnhistory = []
+      @hitcounter = []
+      @ship1_hits = []
+      @ship2_hits = []
+      @ship3_hits = []
+      @ship4_hits = []
+
+    end
+
+
+loop do
+puts "<< ~~~Enemy ships are in the water!~~~ >>"
 puts "(1) Launch a torpedo"
 puts "(2) View your score"
 puts "(3) View where your topedos have landed"
-puts "(4) clear the board and start over"
-puts "(5) Quit"
+puts "(4) Quit"
 user_option = gets.chomp.to_i
 
   case user_option
 
-  when 1
-    ship1_hits = []
-    ship2_hits = []
 
-    puts "Select position (row A-J and a column 1-10) to aim your torpedo at:"
-    loop do
-    torp_position = gets.chomp.to_s
-    binding.pry
-      if board.ship1.include?(torp_position) == true || board.ship2.include?(torp_position) == true
-        puts "launching torpedo!!!! ~~~~~------====>"
-        puts "HIT!!!"
-        puts "**explosion***"
-        ship1_hits << torp_position
-        if @ship1 == ship1_hits
-          puts "YOU SANK A BATTLESHIP!!!!"
-          puts "TEXT ART EXPLOSION"
-        end
-        break
-      elsif board.board.include?(torp_position)
-        puts "launching torpedo!!!! ~~~~~------====>"
-        puts "miss :("
-        break
-      else
-        puts "Please enter a combination of A-J and 1-10:"
-      end
+# Asks the user to enter a positions and returns a hit or a miss
+  when 1
+    if @turnhistory.count == 5
+      puts "<< You have no torpedos left. GAME OVER! >>"
+      exit
     end
 
+    puts "<< Select position (row A-J and a column 1-10) to aim your torpedo at: >>"
+        @torp_position = gets.chomp.to_s
+
+#checks to see if the position was already played
+        if @turnhistory.include?(@torp_position)
+          puts "<< You have already sent a torpedo there >>"
+
+#if not, goes ahead and checks to see if it is a hit
+        elsif
+            if board.ship1.include?(@torp_position) == true
+              puts "<<launching torpedo ~~~~~~-----====>>>"
+              puts "<<HIT!>>"
+              @ship1_hits << @torp_position
+            elsif board.ship2.include?(@torp_position) == true
+              puts "<<launching torpedo ~~~~~~-----====>>>"
+              puts "<<HIT!>>"
+              @ship2_hits << @torp_position
+            elsif board.ship3.include?(@torp_position) == true
+              puts "<<launching torpedo ~~~~~~-----====>>>"
+              puts "<<HIT!>>"
+              @ship3_hits << @torp_position
+            elsif board.ship4.include?(@torp_position)== true
+              puts "<<launching torpedo ~~~~~~-----====>>>"
+              puts "<<HIT!>>"
+              @ship4_hits << @torp_position
+            end
+
+              @turnhistory << @torp_position
+              @hitcounter << @torp_position
+
+              board.board.map! {|x| x == @torp_position? " X": x}
+
+#adds the turn as a hit to the player1s database
+              Player1.create(name: "player1", position: @torp_position, h_or_m: "HIT")
+
+#checks to see if a full ship has been sunk
+              if @ship1_hits.count == 5 || @ship2_hits.count == 5 || @ship3_hits.count == 3 || @ship4_hits.count == 2
+                  puts "<< YOU SANK A SHIP! >>"
 
 
+#checks to see if all ships have been sunk
+              elsif @turnhistory.count == 15
+                  puts "<< YOU SANK ALL ENEMY BATTLESHIPS! CONGRATULATIONS! >>"
+
+              end
+
+#checks to see make sure the position is within the board and returns a miss
+            elsif board.board.include?(@torp_position) == true
+              puts "<< launching torpedo ~~~~~------====>>>"
+              puts "<< miss >>"
+              Player1.create(name: "player1", position: @torp_position, h_or_m: "miss")
+              @turnhistory << @torp_position
+              board.board.map! {|x| x == @torp_position? " O ": x}
+
+            else
+              puts "<< Please enter a combination of A-J and 1-10: >>"
+            end
+
+# Let player view a list of the positions they have slected and if it was a hit or a miss
   when 2
-    puts "Enter player1 or player2 to view their score"
+    puts "<< Enter player1 or player2 to view their score >>"
     player = gets.chomp
-    puts Turns.find_by(name: "player1")
+    if player == "player1"
+    Player1.all.each {|turn| puts "#{Player1.position} : #{Player1.h_or_m}"}
+    elsif player == "player2"
+    Player2.all.each {|turn| puts "#{Player2.position} : #{Player2.h_or_m}"}
+    end
+
+# View the board with hits and misses on it
 
   when 3
-    puts "Enter player1 or player2 to view their torpedos"
-
+    puts board.board[0..19].join("  ")
+    puts board.board[20..39].join("  ")
+    puts board.board[40..59].join("  ")
+    puts board.board[60..79].join("  ")
+    puts board.board[80..99].join("  ")
+    puts board.board[100..119].join("  ")
+    puts board.board[120..139].join("  ")
+    puts board.board[140..159].join("  ")
+    puts board.board[160..179].join("  ")
+    puts board.board[180..199].join("  ")
+    puts board.board[200..219].join("  ")
+    puts board.board[220..239].join("  ")
+    puts board.board[240..259].join("  ")
+    puts board.board[260..279].join("  ")
+    puts board.board[280..299].join("  ")
+    puts board.board[300..319].join("  ")
+    puts board.board[320..339].join("  ")
+    puts board.board[340..359].join("  ")
+    puts board.board[360..379].join("  ")
+    puts board.board[380..399].join("  ")
 
 
   when 4
-    puts "Are you sure you want to clear the board and start over? (yes) or (no)"
-    input = gets.chomp
-      if input == "yes"
-
-      elsif input == "no"
-
-      else
-        puts "Please type yes or no"
-      end
-
-
-  when 5
-    puts "Until we meet again!"
+    puts "<< Until we meet again! >>"
     exit
 
-  end
+  else
+end
+end
--- lib/battleship_models.rb
+++ lib/battleship_models.rb
@@ -1,5 +1,5 @@
 require 'active_record'
-# require 'pry'
+require 'pry'
 
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
@@ -14,17 +14,15 @@ def clean_as_a_whistle
 end
 
 
-class Turn1 < ActiveRecord::Base
-  has_one :player
+class Player1 < ActiveRecord::Base
   has_many :turns
 end
 
-class Turn2 < ActiveRecord::Base
-  has_one :player
+class Player2 < ActiveRecord::Base
   has_many :turns
 end
 
-class Game < ActiveRecord::Base
+class Turn < ActiveRecord::Base
   has_many :players
 end
 
@@ -32,74 +30,86 @@ end
 class CreateGame < ActiveRecord::Migration
 
   def initialize
-    create_table :games do |column|
-      column.string :winner
-    end
-
-    create_table :turn1s do |column|
+    create_table :turns do |column|
       column.string :name
       column.string :position
       column.string :h_or_m
     end
 
-    create_table :turn2s do |column|
+    create_table :players do |column|
       column.string :name
-      column.string :position
-      column.string :h_or_m
     end
 
-  end
-
-end
-
-
-class Player
-  attr_accessor :name, :player_num,
-
-  def initialize name, player_num
-    @name = name
-    @player_num = player_num
   end
 
 end
 
 
 class Board
-  attr_accessor :board, :positions, :size, :ship1, :ship2
+  attr_accessor :board, :positions, :num_players, :ship1, :ship2, :ship3, :ship4
 
-  def initialize size
-    @size = size
+  def initialize
+    @num_players = num_players
 
-      loop do
-      if @size == "small"
-        @x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-        @y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        @positions = @x.product(@y)
-        @board = @positions.map! {|positions| positions.join("")}
-
-        break
-      elsif @size == "large"
         @x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
         @y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         @positions = @x.product(@y)
-        @largeboard = @positions.map! {|positions| positions.join("")}
-
-        break
-      else
-        puts "please enter 'small' or 'large'"
-        @size = gets.chomp
-      end
-
-      @positions = @x.product(@y)
-      @positions = @positions.map! {|positions| positions.join("")}
-
-    end
-
-end
 
 
-  def add_ships size
-    if @size == "small"
+        def print_board
+          @board = @positions.map! {|positions| positions.join("")}
+          puts @board[0..19].join("  ")
+          puts @board[20..39].join("  ")
+          puts @board[40..59].join("  ")
+          puts @board[60..79].join("  ")
+          puts @board[80..99].join("  ")
+          puts @board[100..119].join("  ")
+          puts @board[120..139].join("  ")
+          puts @board[140..159].join("  ")
+          puts @board[160..179].join("  ")
+          puts @board[180..199].join("  ")
+          puts @board[200..219].join("  ")
+          puts @board[220..239].join("  ")
+          puts @board[240..259].join("  ")
+          puts @board[260..279].join("  ")
+          puts @board[280..299].join("  ")
+          puts @board[300..319].join("  ")
+          puts @board[320..339].join("  ")
+          puts @board[340..359].join("  ")
+          puts @board[360..379].join("  ")
+          puts @board[380..399].join("  ")
+
+        end
+
+
+        def print_board2
+          @board2 = @positions.map! {|positions| positions.join("")}
+           puts @board2[0..19].join("  ")
+           puts @board2[20..39].join("  ")
+           puts @board2[40..59].join("  ")
+           puts @board2[60..79].join("  ")
+           puts @board2[80..99].join("  ")
+           puts @board2[100..119].join("  ")
+           puts @board2[120..139].join("  ")
+           puts @board2[140..159].join("  ")
+           puts @board2[160..179].join("  ")
+           puts @board2[180..199].join("  ")
+           puts @board2[200..219].join("  ")
+           puts @board2[220..239].join("  ")
+           puts @board2[240..259].join("  ")
+           puts @board2[260..279].join("  ")
+           puts @board2[280..299].join("  ")
+           puts @board2[300..319].join("  ")
+           puts @board2[320..339].join("  ")
+           puts @board2[340..359].join("  ")
+           puts @board2[360..379].join("  ")
+           puts @board2[380..399].join("  ")
+         end
+       end
+
+
+  def add_ships
+
       @ship1_ary = [["A1", "A2", "A3", "A4", "A5"]]
       @ship1_ary.shuffle!
       @ship1 = @ship1_ary[0]
@@ -108,15 +118,22 @@ end
       @ship2_ary.shuffle!
       @ship2 = @ship2_ary[0]
 
+      @ship3_ary = [["S6", "S7", "S8"]]
+      @ship3_ary.shuffle!
+      @ship3 = @ship3_ary[0]
+
+      @ship4_ary = [["N1", "O1"]]
+      @ship4_ary.shuffle!
+      @ship4 = @ship4_ary[0]
+
       puts "ship1: #{@ship1}"
       puts "ship2: #{@ship2}"
-    end
-    # elsif @size == "large"
-    #   @ship1_ary =
-    #   @ship2_ary =
-    #   @ship3_ary =
-    #   @ship4_ary =
+      puts "ship3: #{@ship3}"
+      puts "ship4: #{@ship4}"
 
-
-  end
 end
+end
+
+
+#
+# binding.pry
